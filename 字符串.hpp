c186db62d057e4eb0f53cc39cbacde889755be2d
zhuzhirui2005 <1743157835@qq.com>
2025-11-05 13:09:02 +0800

    feat(string): add Z function

--- 字符串.hpp
+++ 字符串.hpp
@@ -140,3 +140,25 @@ inline V<int> find_kmp(const V<int> &kmp,const string &s,const string &t){
     }
     return ret;
 }
+
+inline V<int> get_z(const string &s){
+    int n=s.size();
+    V<int>z(n);
+    z[0]=n;
+    for(int i=1,l=0,r=-1;i<n;++i){
+        if(i<=r)z[i]=min(r-i+1,z[i-l]);
+        while(i+z[i]<n&&s[z[i]]==s[i+z[i]])++z[i];
+        if(ckmax(r,i+z[i]-1))l=i;
+    }
+    return z;
+}
+inline V<int> get_ext(const V<int> &z,const string &s,const string &t){
+    int n=s.size(),m=t.size();
+    V<int>ext(n);
+    for(int i=0,l=0,r=-1;i<n;++i){
+        if(i<=r)ext[i]=min(r-i+1,z[i-l]);
+        while(i+ext[i]<n&&ext[i]<m&&t[ext[i]]==s[i+ext[i]])++ext[i];
+        if(ckmax(r,i+ext[i]-1))l=i;
+    }
+    return ext;
+}
